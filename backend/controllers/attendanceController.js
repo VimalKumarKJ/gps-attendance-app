ebcfae37c2c110687695150ac8ef8f059e6a9470
Vimal Kumar .K.J <76762337+kickmyd2602@users.noreply.github.com>
2025-05-24 08:06:08 +0530

    Hotfix for history and today endpoints

--- backend/controllers/attendanceController.js
+++ backend/controllers/attendanceController.js
@@ -1,34 +1,5 @@
 import Attendance from "../models/Attendance.js";
 
-export const getTodayAttendance = async (req, res) => {
-  try {
-    const startOfDay = new Date()
-    startOfDay.setHours(0,0,0,0)
-
-    const endOfDay = new Date()
-    endOfDay.setHours(23,59,59,999)
-
-    const today = Attendance.find({
-      userId: req.user.id,
-      checkInTime: {
-        $gte: startOfDay,
-        $lte: endOfDay,
-      },
-
-    }).sort({createdAt: -1});
-
-    if(!today){
-      return res
-        .status(400)
-        .json({ message: "No attendance record found for today" });
-    }
-
-    res.status(201).json(today);
-  } catch (error) {
-     res.status(500).json({ message: "Server Error", error: err });
-  }
-}
-
 // POST /check-in
 export const checkIn = async (req, res) => {
   const { lat, lng } = req.body;
@@ -78,6 +49,7 @@ export const checkOut = async (req, res) => {
 // GET /history
 export const history = async (req, res) => {
   try {
+    const {from, to} = req.query;
     const query = { userId: req.user.id };
     if (from || to) {
       query.createdAt = {};
@@ -86,7 +58,37 @@ export const history = async (req, res) => {
     }
     const history = await Attendance.find(query).sort({ createdAt: -1 });
     res.status(201).json(history);
-  } catch (error) {
+  } catch (err) {
     res.status(500).json({ message: "Server Error", error: err.message });
   }
 };
+
+// GET /today
+export const getTodayAttendance = async (req, res) => {
+  try {
+    const startOfDay = new Date();
+    startOfDay.setHours(0,0,0,0);
+
+    const endOfDay = new Date();
+    endOfDay.setHours(23,59,59,999);
+
+    const today = await Attendance.findOne({
+      userId: req.user.id,
+      checkInTime: {
+        $gte: startOfDay,
+        $lte: endOfDay,
+      },
+
+    }).sort({createdAt: -1});
+
+    if(!today){
+      return res
+        .status(400)
+        .json({ message: "No attendance record found for today" });
+    }
+
+    res.status(201).json(today);
+  } catch (err) {
+     res.status(500).json({ message: "Server Error", error: err.message });
+  }
+}
